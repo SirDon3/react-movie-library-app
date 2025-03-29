9f4e50a85eca0798be8b4ab1b104eb73c821b9e1
Mohamed Shadhan <shadhanmoahmed13@gmail.com>
2025-03-29 19:28:02 +0200

    movie card added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import MovieCard from './components /MovieCard'
+import './App.css'
+
+function App() {
+
+
+  return (
+ <>
+    <MovieCard movie={{title:"sha's film", year:"2001"}}/>
+    <MovieCard movie={{title:"shadu's film", year:"2003"}}/>
+    <MovieCard movie={{title:"shanana's film", year:"2013"}}/>
+
+    
+ </>
+  )
+}
+
+export default App
