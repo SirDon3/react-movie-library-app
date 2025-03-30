f4873c0017b8df66f4122f2e71cf29cb050c29e9
Mohamed Shadhan <shadhanmoahmed13@gmail.com>
2025-03-30 20:29:13 +0300

    api connected/search implemented

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,34 +1,78 @@
-import MovieCard from "../components/MovieCard";
+import MovieCard from "../components /MovieCard";
 import "../css/Home.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";   
+
 
 function Home() {
-    const [searchQuery, setsearchQuery] = useState("");
-    const movies = [
-        {id: 1, title: "The Shawshank Redemption", year: 1994,},
-        {id: 2, title: "The Godfather", year: 1972,},
-        {id: 3, title: "The Dark Knight", year: 2008,},
-        {id: 4, title: "Minari", year: 2020,}
-    ];
+    const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const handleSearch = (e) => { 
-        e.preventDeafult()
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
+            }
+            catch (err) {
+                console.log(err);
+                setError("Error loading popular movies");
+            }finally {
+                setLoading(false);
+            }
+        }
+        loadPopularMovies();
+    }, []);
+
+    const handleSearch = async (e) => { 
+        e.preventDefault()
+        if (!searchQuery.trim()) return;
+        if(loading) return;
+       
+        setLoading(true);
+
+        try {
+            const searchResults = await searchMovies(searchQuery);
+            setMovies(searchResults);
+            setError(null);
+            console.log("here");
+        }
+        catch (err) {
+            console.log(err);
+            setError("Error loading search results");
+            console.log("here");
    
-    }
+        } finally {
+            setLoading(false);
+            console.log("here");
+        }
+        console.log("here");
+    
+};
 
   return (
     <div className="home">
         <form onSubmit={handleSearch} className="search-form">
             <input type="text" placeholder="Search for a movie..." 
                    className="search-input" value={searchQuery}
-                   onChange={(e) => setsearchQuery(e.target.value)}/>
+                   onChange={(e) => setSearchQuery(e.target.value)}/>
             <button type="submit" className="search-button">Search</button>
         </form>
-     <div className="movies-grid">
+
+    {error && <div className="error-message">{error}</div>}    
+
+    {loading ? (
+        <div className="loading">Loading...</div>
+    ) : (
+        <div className="movies-grid">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
      </div>
+    )}
+ 
     </div>
   );
 }
